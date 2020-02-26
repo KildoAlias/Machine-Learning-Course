960f690420fbf228c11b3d96bed6b59d4b1f3397
KildoAlias <kildo.alias@live.com>
2020-02-26 15:30:43 +0100

    working on classifyboost

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -94,6 +94,7 @@ def classifyBayes(X, prior, mu, sigma):
     # one possible way of finding max a-posteriori once
     # you have computed the log posterior
     h = np.argmax(logProb, axis=0)
+    print(h)
     return h
 
 
@@ -203,6 +204,10 @@ def classifyBoost(X, classifiers, alphas, Nclasses):
         # TODO: implement classificiation when we have trained several classifiers!
         # here we can do it by filling in the votes vector with weighted votes
         # ==========================
+        for i in range(Ncomps):
+            h = classifiers[i].classify(X)
+            for c in range(Nclasses):
+                np.where(h == c, 1, 0)
 
         # ==========================
 
@@ -241,31 +246,31 @@ class BoostClassifier(object):
 #                dataset='iris', split=0.7)
 
 
-#testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='vowel',split=0.7)
+# testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='vowel',split=0.7)
 
 
-#plotBoundary(BoostClassifier(BayesClassifier()), dataset='iris',split=0.7)
+# plotBoundary(BoostClassifier(BayesClassifier()), dataset='iris',split=0.7)
 
 
 # Now repeat the steps with a decision tree classifier.
 
 
-#testClassifier(DecisionTreeClassifier(), dataset='iris', split=0.7)
+# testClassifier(DecisionTreeClassifier(), dataset='iris', split=0.7)
 
 
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
+# testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
 
 
-#testClassifier(DecisionTreeClassifier(), dataset='vowel',split=0.7)
+# testClassifier(DecisionTreeClassifier(), dataset='vowel',split=0.7)
 
 
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='vowel',split=0.7)
+# testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='vowel',split=0.7)
 
 
-#plotBoundary(DecisionTreeClassifier(), dataset='iris',split=0.7)
+# plotBoundary(DecisionTreeClassifier(), dataset='iris',split=0.7)
 
 
-#plotBoundary(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
+# plotBoundary(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
 
 
 # ## Bonus: Visualize faces classified using boosted decision trees
@@ -273,10 +278,10 @@ class BoostClassifier(object):
 # Note that this part of the assignment is completely voluntary! First, let's check how a boosted decision tree classifier performs on the olivetti data. Note that we need to reduce the dimension a bit using PCA, as the original dimension of the image vectors is `64 x 64 = 4096` elements.
 
 
-#testClassifier(BayesClassifier(), dataset='olivetti',split=0.7, dim=20)
+# testClassifier(BayesClassifier(), dataset='olivetti',split=0.7, dim=20)
 
 
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='olivetti',split=0.7, dim=20)
+# testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='olivetti',split=0.7, dim=20)
 
 
 # You should get an accuracy around 70%. If you wish, you can compare this with using pure decision trees or a boosted bayes classifier. Not too bad, now let's try and classify a face as belonging to one of 40 persons!
@@ -290,10 +295,10 @@ class BoostClassifier(object):
 # xTepca = pca.transform(xTe) # apply on test data
 # use our pre-defined decision tree classifier together with the implemented
 # boosting to classify data points in the training data
-#classifier = BoostClassifier(DecisionTreeClassifier(), T=10).trainClassifier(xTrpca, yTr)
-#yPr = classifier.classify(xTepca)
+# classifier = BoostClassifier(DecisionTreeClassifier(), T=10).trainClassifier(xTrpca, yTr)
+# yPr = classifier.classify(xTepca)
 # choose a test point to visualize
-#testind = random.randint(0, xTe.shape[0]-1)
+# testind = random.randint(0, xTe.shape[0]-1)
 # visualize the test point together with the training points used to train
 # the class that the test point was classified to belong to
-#visualizeOlivettiVectors(xTr[yTr == yPr[testind],:], xTe[testind,:])
+# visualizeOlivettiVectors(xTr[yTr == yPr[testind],:], xTe[testind,:])
